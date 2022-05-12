5a855fa031f2e34f1a1ee99adbd4e966a655ea6e
Eduardo Debortoli Piccoli <edudebortoli@icloud.com>
2022-05-12 10:51:53 -0300

    Toastr ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './header/search/search.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, SearchComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/header/search/search.component.ts
+++ src/app/header/search/search.component.ts
@@ -1,63 +1,73 @@
-import { HttpEvent } from '@angular/common/http';
 import { City } from './../../interfaces/city';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { LocationService } from 'src/app/services/location.service';
-
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
-
-  form!: FormGroup
-  cityInput = ''
+  form!: FormGroup;
+  cityInput = '';
   cityData: City = {
     country: '',
     state: '',
     name: '',
     lat: 0,
-    lon: 0
-  }
+    lon: 0,
+  };
 
-  constructor(private location: LocationService) { }
+  constructor(
+    private location: LocationService,
+    private toastr: ToastrService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   async search() {
-    console.log(`searching for: ${this.cityInput}`)
+    console.log(`searching for: ${this.cityInput}`);
 
     this.location.search(this.cityInput).subscribe({
       next: (data: any) => {
-        if(this.fullObject(data)) {
-          data[0] as City
+        if (this.fullObject(data)) {
+          data[0] as City;
           this.cityData = {
             country: data[0].country,
             state: data[0].state,
             name: data[0].name,
             lat: data[0].lat,
-            lon: data[0].lon
-          }
-          this.weatherSearch(this.cityData)
+            lon: data[0].lon,
+          };
+          this.toastr.success(
+            `${this.cityData.name}, ${this.cityData.country} Found!`
+          )
+          this.weatherSearch(this.cityData);
+
+          //success route ->
+
         } else {
-          console.log('error')
+          this.toastr.error(
+            `Try using cityname, country or state. \n
+          Ex: Madrid,ES`,
+            `${this.cityInput} not found`
+          );
         }
-      }
-    })
-    console.log('fim')
+      },
+    });
+    console.log('fim');
   }
 
   private fullObject(object: Object): boolean {
-    const objString = JSON.stringify(object)
+    const objString = JSON.stringify(object);
 
-    if(objString === '[]'){
-      console.log(`cidade não encontrada`)
-      return false
+    if (objString === '[]') {
+      console.log(`cidade não encontrada`);
+      return false;
     } else {
-      console.log(`cidade encontrada`)
-      return true
+      console.log(`cidade encontrada`);
+      return true;
     }
   }
 
